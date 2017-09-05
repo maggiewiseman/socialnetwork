6d10e9e5a73e6df709f208cb5c5cf211e559b4f0
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-05 15:02:43 +0200

    (mew) got submit message

--- src/start.js
+++ src/start.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import axios from 'axios';
 
 
 class Welcome extends React.Component {
@@ -17,14 +17,33 @@ class Welcome extends React.Component {
 }
 
 class Register extends React.Component {
+    submit() {
+        console.log('submitted!');
+        // axios.post('/register', {
+        //     firstName: ''
+        // });
+    }
     render() {
         return (<div className='register-div'>
             <input type="text" name="firstName" placeholder="First Name"/>
             <input type="text" name="lastName" placeholder="Last Name"/>
             <input type="E-mail" name="email" placeholder="E-mail"/>
             <input type="password" name="password" placeholder="Password"/>
-            <button type="submit" name="registerBtn">Submit</button>
+            <button type="submit" name="registerBtn" onClick={e => this.submit(e)}>Submit</button>
         </div>);
+    }
+
+}
+
+class Logo extends React.Component {
+    render() {
+        return (
+            <div>
+                <figure class="main logo">
+                    <img id="logo" src="/img/book_with_cloud_logo.png" alt="logo" />
+                </figure>
+            </div>
+        );
     }
 }
 
