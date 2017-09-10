2950d3059c46cb960a5246a3f5dd553f72e77a66
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-10 14:22:28 +0200

    (mew) styling friends page but using float so might need to clear both...need to try with more than one friend

--- src/friends.js
+++ src/friends.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ProfilePic, PicUploader } from './profile-pic';
 import axios from './axios';
-import { UnderNav, Column } from './styledComponents/wrapper';
+import { Sidebar, MainSection, UnderNav, Column } from './styledComponents/wrapper';
 import { SectionHeader } from './styledComponents/headers';
 import { SidebarMenu, ProfileListItem } from './styledComponents/menus';
 import styled from 'styled-components';
@@ -13,21 +13,60 @@ export default class Friends extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            friends: [],
-            others: []
+            friends: [{
+                id: 1,
+                first_name: 'Phil',
+                last_name: 'Bulldog',
+                profile_pic: 'https://s3.amazonaws.com/maggiesgingersocialnetwork/6xuprnPc76QwWoSGJhDt4BifybxUPfZR.png'
+            }, {
+                id: 2,
+                first_name: 'Mr',
+                last_name: 'Peanutbutter',
+                profile_pic: 'https://s3.amazonaws.com/maggiesgingersocialnetwork/2vZpdN1StHdAf32l-iPQpYJCbGmCHaAQ.png'
+            }],
+            pendingFriends: [{
+                id: 3,
+                first_name: 'Sheryl',
+                last_name: 'Poodle',
+                profile_pic: 'https://s3.amazonaws.com/maggiesgingersocialnetwork/6xuprnPc76QwWoSGJhDt4BifybxUPfZR.png'
+            }, {
+                id: 4,
+                first_name: 'Copper',
+                last_name: 'Wood',
+                profile_pic: 'https://s3.amazonaws.com/maggiesgingersocialnetwork/qe3CFN72NssirxS8-dhCY9dF1DzBmXzZ.png'
+            }]
         };
     }
     render() {
+
+
+        const friend = (
+            <div>
+                <SidePic>
+                    <ProfilePic nav
+                        imgsrc={'https://s3.amazonaws.com/maggiesgingersocialnetwork/6xuprnPc76QwWoSGJhDt4BifybxUPfZR.png'}
+                        first_name={'Phil'}
+                        last_name={'Bulldog'} />
+                </SidePic>
+                <DogInfo>
+                    Phil Bulldog
+                    <Button>End Friendship</Button>
+                </DogInfo>
+
+            </div>
+        );
+
+
+
         return (
             <UnderNav>
                 <Column two>
                     <SidebarMenu>
                         <SectionHeader>
-                            Dogs you might know:
+                            Dogs who want to be friends:
                         </SectionHeader>
                         <ul>
-                            <ProfileListItem>
-                            </ProfileListItem>
+                            {friend}
                         </ul>
                     </SidebarMenu>
                 </Column>
@@ -37,8 +76,7 @@ export default class Friends extends React.Component {
                             Friends:
                         </SectionHeader>
                         <ul>
-                            <ProfileListItem>
-                            </ProfileListItem>
+                            {friend}
                         </ul>
                     </SidebarMenu>
                 </Column>
@@ -46,3 +84,16 @@ export default class Friends extends React.Component {
         );
     }
 }
+
+const SidePic = styled.div`
+    width: 30%;
+    text-align: center;
+    padding: 6px 0;
+    float: left;
+`;
+
+const DogInfo = styled.div`
+    width: 60%;
+    float: left;
+    padding: 6px 0;
+`;
