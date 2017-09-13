6ae5bda12a384c776401de14b53b0f5bdb123a74
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-13 14:34:23 +0200

    (mew) using return on axios to actually return a promise

--- src/actions.js
+++ src/actions.js
@@ -2,19 +2,16 @@ import axios from 'axios';
 
 export function receiveFriends() {
     console.log('ACTION: about to send to axios');
-    axios.get('/api/getFriendships').then((results)=> {
-        console.log('ACTIONS: receiveFriends: ', results);
+    return axios.get('/api/getFriendships').then((results)=> {
+        return {
+            type: 'RECIEVE_FRIENDS',
+            friends: results.data.friends
+        };
 
-        if(results.data.success == 200) {
-            return {
-                type: 'RECIEVE_FRIENDS',
-                friends: results.data.friends
-            };
-        } 
     }).catch(e => {
         console.log(e);
-        this.setState({
-            error: e
-        });
+        return {
+            type: 'ERROR'
+        };
     });
 }
