83923300faa50fc2cebde12f5ff8aeef11c22ee3
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-13 10:54:45 +0200

    (mew) made friends connected

--- src/friends.js
+++ src/friends.js
@@ -1,41 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ProfilePic, PicUploader } from './profile-pic';
-import axios from './axios';
 import { Sidebar, MainSection, UnderNav, Column } from './styledComponents/wrapper';
 import { SectionHeader } from './styledComponents/headers';
 import { SidebarMenu, ProfileListItem } from './styledComponents/menus';
 import styled from 'styled-components';
 import { Button } from './styledComponents/buttons';
 import { HintText } from './styledComponents/text';
+import { receiveFriends } from './actions';
+import { connect } from 'react-redux';
 
-export default class Friends extends React.Component {
+class Friends extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            friends: [{
-                id: 1,
-                first_name: 'Phil',
-                last_name: 'Bulldog',
-                profile_pic: 'https://s3.amazonaws.com/maggiesgingersocialnetwork/6xuprnPc76QwWoSGJhDt4BifybxUPfZR.png'
-            }, {
-                id: 2,
-                first_name: 'Mr',
-                last_name: 'Peanutbutter',
-                profile_pic: 'https://s3.amazonaws.com/maggiesgingersocialnetwork/2vZpdN1StHdAf32l-iPQpYJCbGmCHaAQ.png'
-            }],
-            friendRequests: [{
-                id: 3,
-                first_name: 'Sheryl',
-                last_name: 'Poodle',
-                profile_pic: 'https://s3.amazonaws.com/maggiesgingersocialnetwork/6xuprnPc76QwWoSGJhDt4BifybxUPfZR.png'
-            }, {
-                id: 4,
-                first_name: 'Copper',
-                last_name: 'Wood',
-                profile_pic: 'https://s3.amazonaws.com/maggiesgingersocialnetwork/qe3CFN72NssirxS8-dhCY9dF1DzBmXzZ.png'
-            }]
-        };
+        this.state = {};
+    }
+    componentDidMount() {
+        this.props.dispatch(receiveFriends());
     }
     render() {
         return (
@@ -61,6 +42,45 @@ export default class Friends extends React.Component {
     }
 }
 
+/********* CONNECTED COMPONENT ********/
+
+//the connect function we use below (and that was imported) will pass state to the mapStateToProps function.
+const mapStateToProps = function(state) {
+    return {
+        friends: state.friends
+    };
+};
+
+export default connect(mapStateToProps)(Friends);
+
+
+
+/********* CHILD COMPONENTS **********/
+function FriendList(newFriends) {
+
+    const friendItems = newFriends.map((dog) => {
+        return (
+            <ProfileListItem key={dog.id.toString()}>
+                <SidePic>
+                    <ProfilePic nav
+                        imgsrc={dog.profile_pic}
+                        first_name={dog.first_name}
+                        last_name={dog.last_name}/>
+                </SidePic>
+                <DogInfo><p>{dog.first_name + ' ' + dog.last_name}</p><Button>End Friendship</Button></DogInfo>
+            </ProfileListItem>
+        );
+    });
+
+    return (
+        <ul>
+            {friendItems}
+        </ul>
+    );
+}
+
+/******** STYLED COMPONENTS ***********/
+
 const SidePic = styled.div`
     width: 30%;
     text-align: center;
@@ -77,27 +97,3 @@ const DogInfo = styled.div`
     padding: 10px 0;
 
 `;
-
-function FriendList(newFriends) {
-
-    const friendItems = newFriends.map((dog) => {
-        return (
-            <ProfileListItem key={dog.id.toString()}>
-                <SidePic>
-                    <ProfilePic nav
-                        imgsrc={dog.profile_pic}
-                        first_name={dog.first_name}
-                        last_name={dog.last_name}/>
-                </SidePic>
-                <DogInfo><p>{dog.first_name + ' ' + dog.last_name}</p><Button>End Friendship</Button></DogInfo>
-
-            </ProfileListItem>
-        );
-    });
-
-    return (
-        <ul>
-            {friendItems}
-        </ul>
-    );
-}
