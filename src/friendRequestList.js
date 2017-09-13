1dbc30fc5ec947edcd86e7c237a9d8bfe65f7b79
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-13 15:19:49 +0200

    (mew) moved listmaking magic to separate file

--- src/friendRequestList.js
+++ src/friendRequestList.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { ProfilePic } from './profile-pic';
-import { ProfileListItem } from './styledComponents/menus';
-import styled from 'styled-components';
-import { Button } from './styledComponents/buttons';
 import { connect } from 'react-redux';
+import makeFriendList from './makeFriendList';
+
 
 const PENDING = 1, ACCEPTED = 2, REJECTED = 3, CANCELLED = 4, TERMINATED = 5;
 
@@ -29,46 +27,3 @@ const mapStateToProps = function(state) {
 };
 
 export default connect(mapStateToProps)(FriendRequestList);
-
-
-function makeFriendList(newFriends) {
-    console.log('called make friendList');
-    const friendItems = newFriends.map((dog) => {
-        return (
-            <ProfileListItem key={dog.id.toString()}>
-                <SidePic>
-                    <ProfilePic nav
-                        imgsrc={dog.profile_pic}
-                        first_name={dog.first_name}
-                        last_name={dog.last_name}/>
-                </SidePic>
-                <DogInfo><p>{dog.first_name + ' ' + dog.last_name}</p><Button>End Friendship</Button></DogInfo>
-            </ProfileListItem>
-        );
-    });
-
-    return (
-        <ul>
-            {friendItems}
-        </ul>
-    );
-}
-
-{/******** STYLED COMPONENTS ***********/}
-
-const SidePic = styled.div`
-    width: 30%;
-    text-align: center;
-    display: inline-block;
-    margin: 0;
-    padding: 6px 0;
-`;
-
-const DogInfo = styled.div`
-    width: 60%;
-    display: inline-block;
-    vertical-align: top;
-    margin: 0;
-    padding: 10px 0;
-
-`;
