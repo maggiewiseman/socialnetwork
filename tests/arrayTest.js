080c3ce5f8eade714f532cb72146fb87384e9ce4
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-20 14:42:49 +0200

    (mew) initial idea for handler tests

--- tests/arrayTest.js
+++ tests/arrayTest.js
@@ -4,6 +4,6 @@ var assert = chai.assert;
 describe('Array', function() {
     it('should start empty', function() {
         var arr = [];
-        assert.equal(arr.length, 0);
+        assert.equal(arr.length, 0, 'Array length was not 0');
     });
 });
---
+++ tests/handlerTest.js
@@ -0,0 +1,11 @@
+
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+
+
+describe('handle searchName', function() {
+    it('should return user_id, first_name and last_name for a string that matches the first_name or user_name in the database');
+    
+    it('should return return an empty array if the string does not exist in the database');
+
+})
