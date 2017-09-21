88552251c4739d10890ce5e6af2ba0751968cd99
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-21 17:46:31 +0200

    (mew) initial wallpost component

--- src/wallposts.js
+++ src/wallposts.js
@@ -3,16 +3,18 @@ import { connect } from 'react-redux';
 import axios from './axios';
 import { getWallPosts, saveWallPosts } from './actions';
 import { HintText } from './styledComponents/text';
-
+import { Button } from './styledComponents/buttons';
+import styled from 'styled-components';
 
 
 class WallPosts extends React.Component {
     constructor(props) {
         super(props);
         this.handleInput = this.handleInput.bind(this);
+        this.savePost = this.savePost.bind(this);
     }
     componentDidMount() {
-        // axios.post('/getWallPosts').then((results) => {
+        // axios.post('/api/wallposts/' + this.props.currUser.id).then((results) => {
         //     this.props.dispatch(getWallPosts(results));
         // });
     }
@@ -20,6 +22,9 @@ class WallPosts extends React.Component {
         this.setState({
             currPost: e.target.value
         });
+    }
+    savePost() {
+        this.props.dispatch(saveWallPosts(this.state.currPost));
     }
     render() {
         var taStyle = {
@@ -30,7 +35,11 @@ class WallPosts extends React.Component {
             <div>
                 <h3>Wall Posts</h3>
                 <HintText>Say something! </HintText>
-                <textarea style={taStyle} rows='4' onChange={this.handleInput}></textarea>
+                <EditField>
+                    <textarea style={taStyle} rows='4' onChange={this.handleInput}></textarea>
+                    <Button onClick={this.savePost}>Post</Button>
+                    {this.props.posts && <div>{this.props.posts} POSTS!!! </div>}
+                </EditField>
             </div>
         );
     }
@@ -42,8 +51,21 @@ class WallPosts extends React.Component {
 {/*the connect function we use below (and that was imported) will pass state to the mapStateToProps function. */}
 const mapStateToProps = function(state) {
     return {
+        currUser: state.currUser,
         posts: state.posts
     };
 };
 
 export default connect(mapStateToProps)(WallPosts);
+
+{/********* STYLED COMPONENTS ********/}
+export const EditField = styled.div`
+    margin-left: 20px;
+
+    > div {
+        > textarea {
+            display: block;
+
+        }
+    }
+`;
