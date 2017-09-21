829cc338cc3e9138d0a2b7fafed9e61db8d63e4b
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-21 17:44:54 +0200

    (mew) initial post and get routes for wall posts

--- routers/wallPostRoutes.js
+++ routers/wallPostRoutes.js
@@ -3,12 +3,25 @@ var dbQuery = require('../dbQuery');
 
 const wallPostRoutes = (app) => {
     //create wallPost
-    app.post('/api/wallPost/:id', (req,res) => {
+    app.post('/api/wallpost/:id', (req,res) => {
         let info = [req.session.user.id, req.params.id, req.body.text, req.body.image, req.body.link, req.body.link_text];
         return dbQuery.addPost(info).then(() => {
             return res.json({
-                status: 200,
                 success: true
+            });
+        }).catch(e => {
+            return res.json({
+                error: e
+            });
+        });
+    });
+
+    app.get('/api/wallpost/:id', (req, res) => {
+        let info = [req.params.id];
+        return dbQuery.getWallpostsByUserId(info).then((results) => {
+            return res.json({
+                success: true,
+                posts: results.rows
             });
         }).catch(e => {
             return res.json({
