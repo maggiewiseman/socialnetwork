209454cce85750941aa31c3d33c88c67290d7e83
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-21 21:55:08 +0200

    (mew) started my version of tdd for adding posts

---
+++ src/addPost.js
@@ -0,0 +1,4 @@
+/*
+so I need to figure out if a post button should appear or not. So when a user goes to a user profile, there should be a friends status.
+    if friend status is accepted / if "word"   "End", then the add post section can appear. 
+*/
